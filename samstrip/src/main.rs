d4b3bb9a22be30fb8ff378e61b263df893ca2fcc
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2024-11-20 10:22:49 +0100

    Samstrip: More performance improvements
    Wrap stdin and stdout in buffers, and use memchr crate to find tab-sep fields.

--- samstrip/src/main.rs
+++ samstrip/src/main.rs
@@ -1,4 +1,33 @@
-use std::io::{self, BufRead, Write};
+use std::io::{self, BufRead, BufReader, BufWriter, Write};
+
+struct FieldIterator<'a> {
+    bytes: Option<&'a [u8]>,
+}
+
+impl<'a> FieldIterator<'a> {
+    fn new(s: &'a [u8]) -> Self {
+        Self { bytes: Some(s) }
+    }
+}
+
+impl<'a> Iterator for FieldIterator<'a> {
+    type Item = &'a [u8];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(mem) = self.bytes {
+            if let Some(next_pos) = memchr::memchr(b'\t', mem) {
+                let (res, new) = mem.split_at(next_pos);
+                // Safety: New is guaranteed to be nonempty by the semantics of split_at
+                unsafe { self.bytes = Some(new.get_unchecked(1..new.len())) }
+                Some(res)
+            } else {
+                self.bytes.take()
+            }
+        } else {
+            None
+        }
+    }
+}
 
 fn main() {
     if let Some(arg) = std::env::args().nth(1) {
@@ -13,43 +42,64 @@ fn main() {
             std::process::exit(1)
         }
     }
-    let mut stdout = io::stdout().lock();
-    let mut stdin = io::stdin().lock();
+    let mut stdout = BufWriter::new(io::stdout().lock());
+    let mut stdin = BufReader::new(io::stdin().lock());
+    match stdin.fill_buf().unwrap().first().map(|&b| b == b'@') {
+        Some(true) => {}
+        Some(false) => {
+            eprintln!(
+                "Error: First SAM line did not start with a @, indicating a missing header. \
+                \nDid you remember to pass in the whole SAM file? \
+                If producing the SAM file using `samtools view`, remember the `-h` flag to include the header."
+            );
+            std::process::exit(1);
+        }
+        None => std::process::exit(0),
+    }
+
     let mut line: Vec<u8> = Vec::new();
-    let mut to_write: Vec<u8> = Vec::new();
     let mut n_bytes = stdin.read_until(b'\n', &mut line).unwrap();
     if line.first().map(|&b| b != b'@').unwrap_or(true) {
         eprintln!(
             "Error: First SAM line did not start with a @, indicating a missing header. \
-            \nDid you remember to pass in the whole SAM line? \
+            \nDid you remember to pass in the whole SAM file? \
             If producing the SAM file using `samtools view`, remember the `-h` flag to include the header."
         );
         std::process::exit(1);
     }
     while n_bytes > 0 {
+        // TODO: We could speed this up by copying the header in a separate loop
         if line.first().map(|&b| b == b'@').unwrap_or(true) {
             stdout.write_all(&line).unwrap();
             line.clear();
             n_bytes = stdin.read_until(b'\n', &mut line).unwrap();
             continue;
         }
-        let mut fields = line.split(|&b| b == b'\t');
-        if let Some(field) = fields.next() {
-            to_write.extend_from_slice(field);
+        // Write the line from the start to the 9th tab, as the first 9 fields
+        // should be copied over unchanged
+        let start = memchr::memchr_iter(b'\t', &line).nth(8).unwrap_or_else(|| {
+            eprintln!("Error: In SAM alignment line, did not see all required fields");
+            std::process::exit(1)
+        });
+        // The 10th and 11th field are SEQ and QUAL - we replace them with *
+        stdout.write_all(&line[..=start]).unwrap();
+        stdout.write_all(b"*\t*").unwrap();
+        // Loop over the last fields - skip the first two SEQ and QUAL fields which we replaced above,
+        // and then look for the NM:i field which should always be present. If we find it, copy it
+        // to the out buffer.
+        let fields = FieldIterator::new(&line[start + 1..]);
+        let need_newline = if let Some(nm) = fields.skip(2).find(|&b| b.starts_with(b"NM:i")) {
+            stdout.write_all(b"\t").unwrap();
+            stdout.write_all(nm).unwrap();
+            nm.last().map(|&b| b != b'\n').unwrap_or(true)
+        } else {
+            true
+        };
+        // The NM field which we copied might have ended in a newline, so check for existing newline
+        // before adding one.
+        if need_newline {
+            stdout.write_all(b"\n").unwrap();
         }
-        for field in fields.by_ref().take(8) {
-            to_write.push(b'\t');
-            to_write.extend_from_slice(field);
-        }
-        to_write.extend_from_slice(b"\t*\t*\t");
-        if let Some(nm) = fields.skip(2).filter(|s| s.starts_with(b"NM")).next() {
-            to_write.extend_from_slice(nm);
-        }
-        if !to_write.last().map(|&b| b == b'\n').unwrap_or(false) {
-            to_write.push(b'\n')
-        }
-        stdout.write_all(&to_write).unwrap();
-        to_write.clear();
         line.clear();
         n_bytes = stdin.read_until(b'\n', &mut line).unwrap();
     }
@@ -69,4 +119,9 @@ Options:
     -h --help: Print help
     -V --version: Print version
 
-Example usage: `samtools view -h file.bam | samstrip | samtools view -b - > stripped.bam`";
+Example usage:
+Stripping an exiting BAM file:
+`samtools view -h file.bam | samstrip | samtools view -b - > stripped.bam`
+
+Stripping a BAM file while creating it:
+`minimap2 -ax sr ref.fa fw.fq rv.fq | samstrip | samtools view -b - > file.bam`";
